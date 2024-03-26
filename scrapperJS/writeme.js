7b85a279bb6798fcf472165e15f1081f7b133f94
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2024-03-26 15:16:40 +0000

    a script to write into a file

---
+++ scrapperJS/writeme.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { error } = require("console");
+const fs = require("fs");
+
+// get the file path and content
+const filePath = process.argv[2]
+const content = process.argv[3]
+
+fs.writeFile(filePath, content, "utf-8", (error) => {
+    // display error message if it occurs
+    if (error) {
+        console.error(error);
+    }
+
+    // display success message
+    console.log(`Content has been written to ${filePath}`);
+})
