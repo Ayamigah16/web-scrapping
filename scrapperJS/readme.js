996cf61e92cb56a89c84b21ed54f490fbe93d11b
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2024-03-26 14:57:16 +0000

    script to read file contents

---
+++ scrapperJS/readme.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { error } = require("console");
+const fs = require("fs");   // import file system module
+
+const filepath = process.argv[2]
+
+fs.readFile(filepath, 'utf-8', (error, data) => {
+    // print error message if occurs
+    if (error) {
+        console.error(error);
+        return
+    }
+    
+    // print the content of the file
+    console.log(data)
+})
