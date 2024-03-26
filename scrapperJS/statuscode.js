f40c61045318ffda96b889e9a733133fa8583df0
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2024-03-26 15:34:40 +0000

    display status code

---
+++ scrapperJS/statuscode.js
@@ -0,0 +1,20 @@
+// A script that display the status code of a GET request.
+
+"use strict";
+
+// import the request module
+const request = require('request')
+
+// get the url
+const url = process.argv[2]
+
+// make a get request
+request.get(url, (error, response) => {
+    // display error if occurs
+    if (error) {
+        console.error(error);
+    }
+
+    // print the status code
+    console.log(`status_code: ${response.statusCode}`);
+})
